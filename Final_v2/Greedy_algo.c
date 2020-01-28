4c1fa456175c125304c26fa70b60c5a271d84608
Limouna <58744759+Limouna@users.noreply.github.com>
2020-01-29 00:11:21 +0100

    Update Greedy_algo.c

--- Final_v2/Greedy_algo.c
+++ Final_v2/Greedy_algo.c
@@ -1,176 +1,119 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include "fastq.h"
+#include "Greedy_Algo.h"
 
-int nbre_lignes(char* file_dir);
-void extract_file(char* file_dir, char** sequence);
-int nbre_read(char* file_dir);
-int taille_read(char* file_dir);
-char* concatenate(char *r1, char *r2, int best_olen, int max_size,char* temp);
-void adj_matrix(char** sequence, int n, int read, int max_size);
-int num_of_overlap(char *a,char *b);
-void join(char **sequence,char* shortest_common_superstring, int n);
-
-void main()
+char* assemblage()
 {
+	clock_t tic = clock();
     char** sequence;
-    char* shortest_common_superstring;
-    int n,read ;
-    int max_size;
+    char** finalscs;
+    char* scs1;
+    int n,read;
+    int recherche;
     n = nbre_read("C:\\Users\\Msi\\Desktop\\genome.fq");
     read = taille_read("C:\\Users\\Msi\\Desktop\\genome.fq");
-    max_size = read * n;
+    scs1 = malloc(n*read*sizeof(char));
     sequence = malloc(n*sizeof(char*));
     for(int i=0; i < n; i++)
-        sequence[i] = malloc(max_size*sizeof(char));
-    shortest_common_superstring = malloc(max_size * sizeof(char));
-    strcpy(shortest_common_superstring,"");
-    extract_file("C:\\Users\\Msi\\Desktop\\genome.fq",sequence);
-    adj_matrix(sequence,n,read,max_size);
+        sequence[i] = malloc(read*sizeof(char));
+    finalscs = malloc(n*sizeof(char*));
     for(int i=0; i < n; i++)
-        printf("%s,", sequence[i]);
-    join(sequence, shortest_common_superstring,n);
+        finalscs[i] = malloc(read*sizeof(char));
+    extract_file("C:\\Users\\Msi\\Desktop\\genome.fq",sequence);
+    //adj_matrix(sequence,n,read,max_size);
+    //for(int i=0; i < n; i++)
+    //    printf("%s,", sequence[i]);
+
+    strcpy(scs1,chemin(sequence, n, read, finalscs));
+    //join(sequence, shortest_common_superstring,n);
     printf("\n \n");
-    printf("shortest common superstring = %s",shortest_common_superstring);
-    //free_mem(sequence,n);
+    //printf("shortest common superstring = %s",shortest_common_superstring);
+    //printf("final = %s \n \n %s",sequence[0],sequence[1]);
+    //rechercherFromReads(sequence,read,n);
+    /*for (int i = 0; i < n; ++i)
+    {
+        printf("%s \n",finalscs[i]);
+    }*/
     printf("\n");
-    system("pause");
-
+    clock_t toc = clock();
+    printf("le genome assemblé est: %s", scs1);
+    printf(" \ntemps d'éxecution: %f seconds\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+    return scs1;
 }
 
-void extract_file(char* file_dir,char** sequence)
+
+char* chemin(char** sequence, int n, int read, char** finalscs)
 {
-    int n , read , i , m , nbre_read ;
-    char c , **copie ;
-    FILE *fichier;
-    //nombre de reads
-    n = nbre_lignes(file_dir);
-    m=n*2;
-    //taille d'un read
-    fichier = fopen(file_dir,"r");
-    c = getc(fichier);
-    while (c!= '\n')
-        c = getc(fichier);
-    read = 0 ;
-    c = getc(fichier);
-    while (c!= '\n')
-    {
-         c = getc(fichier);
-         read++;
-    }
-
-    copie = malloc(m*sizeof(char*));
-    for (int i=0; i < m ; i++)
-        copie[i] = malloc(read*sizeof(char));
-
-    i=0;
-    rewind(fichier);
-    while (fgets(copie[i],101,fichier)!= NULL)
-    {
-        i++;
-    }
-    nbre_read = n/4 ;
-    strcpy(sequence[0],copie [1]);
-    for (int i=0; i < nbre_read; i++)
-    {
-        strcpy(sequence[i],copie[1+6*i]);
-    }
-    fclose(fichier);
-    free(copie);
-}
-
-int nbre_lignes(char* file_dir)
-{
-	FILE *fichier;
-	int count = 0;
-	char c;
-
-    fichier = fopen(file_dir ,"r");
-
-	if (fichier == NULL)
-	{
-		printf("Could not open file");
-		return 0;
-	}
-
-	for (c = getc(fichier); c != EOF; c = getc(fichier))
-		if (c == '\n')
-			count = count + 1;
-
-	fclose(fichier);
-
-	return count;
-}
-
-
-int nbre_read(char* file_dir)
-{
-    int n;
-    n = nbre_lignes(file_dir);
-    return n/4;
-}
-
-int taille_read(char* file_dir)
-{
-    FILE* fichier;
-    int c,read;
-    fichier = fopen(file_dir,"r");
-    c = getc(fichier);
-    while (c!= '\n')
-        c = getc(fichier);
-    read = 0 ;
-    c = getc(fichier);
-    while (c!= '\n')
-    {
-         c = getc(fichier);
-         read++;
-    }
-    return read;
-}
-
-void adj_matrix(char** sequence, int n, int read, int max_size){
-    int n2 = n*n;
-    int olen,best_olen = 0;
-    char *no = "no";
-    char *r1,*r2,*temp;
-    int c,d;
-    int check = 0;
-    int cnt = 0;
-    best_olen = 1;
-    temp = (char *)malloc(max_size * sizeof(char));
-    while(best_olen > 0){
-        best_olen = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if (strcmp(sequence[i],no) !=0 && strcmp(sequence[j],no) !=0){
-                    olen = num_of_overlap(sequence[i],sequence[j]);
-                    if((olen >= best_olen) && (i!=j)){
-                        best_olen = olen;
-                        c = i;
-                        d = j;
-                        check = 1;
-                    }
+    int max,i1,j1,cnt;
+    int *p;
+    int c=0;
+    char* scs;
+    char* temp;
+    int *indices = malloc(2*(n+1)*sizeof(int));
+    scs = malloc(n*read*sizeof(char));
+    temp = malloc(n*read*sizeof(char));
+    strcpy(scs,"");
+    p = graph(sequence,n);
+    cnt = 0;
+    while(cnt<n){
+        max = 0;
+        for(int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (( *(p + n * i + j) >= 0 ) && (*( p + n * i + j) >= max))
+                {
+                    max = *( p + n * i + j);
+                    i1 = i;
+                    j1 = j;
                 }
             }
         }
-        if (check == 1){
-            printf("r1 = %s \n r2 = %s \n",sequence[c],sequence[d]);
-            concatenate(sequence[c],sequence[d],best_olen,max_size,temp);
-            strcpy(sequence[c],temp);
-            strcpy(sequence[d],no);
-            printf("R = %s \n best_olen %d i=%d, j=%d  \n",sequence[c],best_olen,c,d);
-        }
-        cnt += 1;
-        printf("cnt = %d \n",cnt);
-        if(best_olen == 0){
-            free(temp);
-            break;
-        }
+        remove_edge(p,i1,j1,n);
+        printf("arc choisi %d-->%d \n",i1,j1);
+        indices[2*cnt] = i1;
+        indices[2*cnt+1] = j1;
+        //printf("cnt = %d \n",cnt);
+        cnt++;
     }
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d-->%d,",indices[2*i],indices[2*i+1]);
+    }
+    printf("\n DONE");
+    strcpy(finalscs[0],sequence[indices[0]]);
+    strcpy(finalscs[1],sequence[indices[1]]);
+    for (int j = 1; j < n-1; j++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if(indices[2*i]==indices[2*c+1])
+            {
+                c=i;
+                break;
+            }
+        }
+        strcpy(finalscs[j+1],sequence[indices[2*c+1]]);
+    }
+    printf("\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("seq=%s \n",finalscs[i+1]);
+    }
+    strcat(scs,finalscs[0]);
+    for (int i = 0; i < n-1; i++)
+    {
+        concatenate(scs,finalscs[i+1],temp);
+        strcpy(scs,temp);
+    }
+    return scs;
 }
 
-int num_of_overlap(char *a,char *b){
+int num_of_overlap(char *a,char *b)
+{
     int na,nb;
     int j=0;
     int i=0;
@@ -179,16 +122,19 @@ int num_of_overlap(char *a,char *b){
     nb=strlen(b);
 
     while(i<na){
-        if(b[j]==a[i]){
+        if(b[j]==a[i])
+        {
             j=j+1;
             i=i+1;
             boo = 0;
         }
-        else if (boo==0){
+        else if (boo==0)
+        {
             j=0;
             boo=1;
         }
-        else {
+        else
+        {
             j=0;
             i=i+1;
             boo = 1;
@@ -197,26 +143,51 @@ int num_of_overlap(char *a,char *b){
     return j;
 }
 
-char* concatenate(char *r1, char *r2, int best_olen, int max_size, char *temp)
+void concatenate(char *r1, char *r2,char* temp)
 {
     int len1 = strlen(r1);
     int len2 = strlen(r2);
+    int olen = num_of_overlap(r1,r2);
     strcpy(temp,r1);
-    for (int i=0; i< (len2 - best_olen); i++){
-        temp[len1 + i] = r2[best_olen + i];
+    for (int i=0; i< (len2 - olen); i++){
+        temp[len1 + i] = r2[olen + i];
     }
-    temp[len1 + len2 - best_olen] = '\0';
-    //printf("%d \n %s \n %s \n %s ",best_olen,r1,r2);
-    //free(r1);
-    //printf("%s",temp);
+    temp[len1 + len2 - olen] = '\0';
 }
 
 
-void join(char **sequence,char* shortest_common_superstring, int n)
+int* graph( char** sequence, int n){
+    int n2 = n*n;
+    int *p;
+    //int *adj_mat = (int *)malloc(n2 * sizeof(int));
+    p = calloc(n2, sizeof(int) );
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j){
+                *(p+n*i+j) = -1;
+            }
+            //printf("%d,",num_of_overlap(sequence[i],sequence[j]));
+            else{
+                *(p+n*i+j) = num_of_overlap(sequence[i],sequence[j]);
+                //printf("%d, ",*(p+n*i+j)); //TO PRINT ADJACENCY MATRIX
+            }
+        }
+    }
+    printf("building graph done \n");
+    return p;
+}
+
+void remove_edge(int *p, int i1,int j1, int n)
 {
-    char *no = "no";
-    for(int i=0; i<n; i++){
-        if(strcmp(sequence[i],no) != 0)
-            strcat(shortest_common_superstring,sequence[i]);
+    *(p + n*i1 + j1) = -1;
+    *(p + n*j1 + i1) = -1;
+    for (int i = 0; i < n; i++)
+    {
+        *(p + n*i + j1) = -1;
+    }
+    for (int j = 0; j < n; j++)
+    {
+        *(p + n*i1 + j ) = -1;
     }
 }
+
