466d3e4399848d6357183c92e5f2e4b5ce1102ac
Limouna <58744759+Limouna@users.noreply.github.com>
2020-01-29 00:11:50 +0100

    Update Rechercher.c

--- Final_v2/Rechercher.c
+++ Final_v2/Rechercher.c
@@ -1,10 +1,21 @@
-void rechercherFromReads(char** sequence, int n, int nbrReads){
-    //rechercherFromReads(sequence,read,n);
-    char* motif ;
+#include <stdio.h>
+#include <stdlib.h>
+#include "Rechercher.h"
+#include "fastq.h"
+#include <stdbool.h>
+
+
+bool rechercherFromReads(char* motif, char* file_dir){
+    bool d;
     int i = 0;
-    motif  = malloc((n+1)*sizeof(char));
-    printf("Entrer un motif : \n");
-    scanf("%s", motif);
+    char** sequence;
+    int nbrReads = nbre_read(file_dir);
+    int n = taille_read(file_dir);
+    sequence = malloc(nbrReads*sizeof(char*));
+    for(int i=0; i < nbrReads; i++)
+        sequence[i] = malloc(n*sizeof(char));
+    extract_file(file_dir,sequence);
+    //rechercherFromReads(sequence,read,n);
     while(strstr(sequence[i],motif) == NULL && i < nbrReads-1){
         //printf("%d\n",strstr(sequence[i],motif) );
         //printf("i=%d,", i)
@@ -12,11 +23,14 @@ void rechercherFromReads(char** sequence, int n, int nbrReads){
     }
     if(i < nbrReads-1 || strstr(sequence[i],motif) != NULL ){
         printf("TROUVEE dans le read num %d",i);
+        d = true;
 
          //Motif trouve
     }
     else{
         printf("Motif non trouve");
+        d = false;
         //Motif non trouve
     }
+    return d;
 }
